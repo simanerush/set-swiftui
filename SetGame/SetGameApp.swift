78348d2631c9bc2520a44b3eb32f01da018fd2cf
Sima Nerush <2002ssn@gmail.com>
2021-12-28 17:01:13 -0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Serafima Nerush on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
